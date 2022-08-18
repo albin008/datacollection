fa16512359b43d4a2d74cbb67334295379ee0d8d
albin008 <albin.michael800@gmail.com>
2022-08-18 22:05:49 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express=require('express')
+const app=express()
+const port=2000
+const {Client}=require('pg')
+
+const client=new Client({
+  host:"localhost",
+  user:"postgres",
+  port:5432,
+  password:"123456",
+  database:"wasi"
+})
+
+client.connect()
+
+// client.query('select * from userdetails',(error1,response)=>
+// {
+//     if(!error1)
+//     {
+//         console.log(response.rows);
+//     }
+// })
+
+app.listen(port,()=>{
+  console.log(`on ${port} is sucessfully running`)
+})
+
+app.use(express.json())
+
+app.use(function(req, res, next) 
+{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST ,OPTIONS ,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if ('OPTIONS' == req.method) {
+       res.sendStatus(200);
+     }
+     else {
+       next();
+}});
+
+app.get('/micheal',(req,res)=>
+{     
+    res.status(200).send('<h1>albin micheal</h1>')
+})
+
+app.get('/albin',(req,res)=>{
+  res.json({name:"albin",age:21,place:"ekm"})
+})
+
+app.get('/getdata',(req,res)=>{
+  client.query('select * from userdetails',(err,responsequery)=>
+  {
+      res.json(responsequery.rows)
+  })
+})
+
+
+app.post('/vishnuajay',(req,res)=>{
+  console.log(req.body)
+  client.query(`insert into userdetails values
+  ('${req.body.fullname}','${req.body.username}',
+  '${req.body.password}','${req.body.phonenumber}',
+  '${req.body.email}')`)
+})
+
+// let abc='ab=ujushvavh'
+// `${abc} is my name`
